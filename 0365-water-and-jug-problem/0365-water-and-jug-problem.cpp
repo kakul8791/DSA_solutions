86aaded7769f7dce04572f049ee207985cae0d54
kakul rathi <rathikakul4@gmail.com>
2026-07-03 20:51:10 +0530

    Time: 0 ms (100%), Space: 7.7 MB (93%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool canMeasureWater(int x, int y, int target) {
+        if (target > x + y) {
+            return false;
+        }
+
+        return target % gcd(x, y) == 0;
+    }
+};
