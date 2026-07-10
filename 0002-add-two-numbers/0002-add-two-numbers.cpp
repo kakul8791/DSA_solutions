e961a22f41664742cad64f1e79e24378f49e52a1
kakul rathi <rathikakul4@gmail.com>
2026-07-10 12:09:06 +0530

    Time: 2 ms (39.08%), Space: 77.2 MB (45.52%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,28 +11,27 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *dummy= new ListNode(-1);          // answer list ka starting helper node
-        ListNode* tail = dummy;    // jahan next answer node attach hoga
-        int carry = 0;
+        ListNode *dummy= new ListNode(-1);
+        ListNode *tail=dummy;
+        int carry=0;
+        while(l1!=nullptr || l2!=nullptr || carry!=0){
+            int sum=carry;
+            if(l1!=nullptr){
+                sum+=l1->val;
+                l1=l1->next;
 
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int sum = carry;
-
-            if (l1 != nullptr) {
-                sum += l1->val;
-                l1 = l1->next;
             }
-
-            if (l2 != nullptr) {
-                sum += l2->val;
-                l2 = l2->next;
+            if(l2!=nullptr){
+                sum+=l2->val;
+                l2=l2->next;
             }
+            carry=sum/10;
+            tail->next= new ListNode(sum%10);
+            tail=tail->next;
 
-            carry = sum / 10;               // 10 ya usse zyada hua toh carry
-            tail->next = new ListNode(sum % 10); // current digit
-            tail = tail->next;
+
         }
-
-        return dummy->next; // dummy actual answer ka part nahi hai
+        return dummy->next;
+        
     }
 };
