ec43c61a98b6b45dc726e25a9534e31e8e2254ad
kakul rathi <rathikakul4@gmail.com>
2026-08-04 20:42:21 +0530

    Time: 7 ms (18.86%), Space: 33.8 MB (8.08%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
+        int min_ele = *min_element(nums.begin(),nums.end());
+        int max_ele = *max_element(nums.begin(),nums.end());
+        unordered_set<int>st(nums.begin(),nums.end());
+        vector<int>missing;
 
-        // Store all elements in a hash set
-        unordered_set<int> st(nums.begin(), nums.end());
-
-        // Find minimum and maximum element
-        int minVal = *min_element(nums.begin(), nums.end());
-        int maxVal = *max_element(nums.begin(), nums.end());
-
-        vector<int> missing;
-
-        // Check every number between min and max
-        for (int i = minVal + 1; i < maxVal; i++) {
-
-            // If number is not present, add it
-            if (st.find(i) == st.end()) {
+        for(int i=min_ele+1;i<max_ele;i++){
+            if(st.find(i)==st.end()){
                 missing.push_back(i);
             }
         }
-
         return missing;
+        
     }
 };
