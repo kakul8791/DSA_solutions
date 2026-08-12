200e85a948460e5a5c3cfa08408a625e3d7a96df
kakul rathi <rathikakul4@gmail.com>
2026-08-12 15:15:05 +0530

    Time: 0 ms (100%), Space: 15.7 MB (51.39%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -6,14 +6,17 @@ public:
         while(i>=0 && nums[i]>=nums[i+1]){
             i--;
         }
+
         if(i>=0){
             int j=n-1;
+
             while(nums[j]<=nums[i]){
                 j--;
             }
             swap(nums[i],nums[j]);
         }
         reverse(nums.begin()+i+1,nums.end());
+
         
     }
 };
