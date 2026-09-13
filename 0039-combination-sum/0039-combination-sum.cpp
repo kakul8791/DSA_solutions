effbed1f38c398dff7bf74adfa58ecc484070eaf
kakul rathi <rathikakul4@gmail.com>
2026-09-13 20:12:28 +0530

    Time: 4 ms (36.47%), Space: 14.2 MB (58.03%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-void findCombination(int index,int target,vector<int>ds,vector<vector<int>>&ans,vector<int>&arr){
-    int n=arr.size();
-    if(index==n){
+void func(int index, vector<int>&nums,int target, vector<int>&ds,vector<vector<int>>&ans){
+    if(index==nums.size()){
         if(target==0){
             ans.push_back(ds);
             
         }
         return;
     }
-    if(arr[index]<=target){
-        ds.push_back(arr[index]);
-        findCombination(index,target-arr[index],ds,ans,arr);
+    if(nums[index]<=target){
+        ds.push_back(nums[index]);
+        func(index,nums,target-nums[index],ds,ans);
         ds.pop_back();
-        
     }
-    findCombination(index+1,target,ds,ans,arr);
-    
+
+    func(index+1,nums,target,ds,ans);
 }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         vector<int>ds;
         vector<vector<int>>ans;
-        findCombination(0,target,ds,ans,candidates);
+        func(0,nums,target,ds,ans);
         return ans;
-
         
     }
 };
